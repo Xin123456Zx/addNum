dede340a5d85c88ae16056821f18054a0268f8f0
Xin Zhao <309460108@qq.com>
2018-03-01 16:12:16 -0500

    add1
    add 2 numbers

---
+++ src/addNumbers/addNumbers.java
@@ -0,0 +1,14 @@
+package addNumbers;
+
+public class addNumbers 
+{
+	static public int addNum(int a, int b)
+	   {
+	      return a * b;
+	   }
+	 
+	 public static void main(String[] args)
+	 {
+		 
+	 }
+}
---
+++ src/addNumbers/addNumbersTest.java
@@ -0,0 +1,25 @@
+package addNumbers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class addNumbersTest {
+
+	@Test
+	void test() {
+		assertEquals(10,addNumbers.addNum(5, 5));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
